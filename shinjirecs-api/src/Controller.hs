2841d24c5ecae5b77874fe6ec4ae2b42005176b5
Shinjiro Itagaki <ishinjiroster@gmail.com>
2017-02-11 04:39:23 +0900

    modify controller

--- shinjirecs-api/src/Controller.hs
+++ shinjirecs-api/src/Controller.hs
@@ -1,14 +1,23 @@
 module Controller where
 import DB
 import Web.Scotty (ActionM)
-import Control.Monad.IO.Class(MonadIO) -- base
-import Database.Persist.Sql(ConnectionPool,SqlPersistT)  --persistent
-
+import Control.Monad.IO.Class(liftIO,MonadIO) -- base
+import Database.Persist.Sql(ConnectionPool,SqlPersistT, runSqlPool)  --persistent
+import Database.Persist (PersistEntity (..)) --persistent
+import qualified Database.Persist.Class as PS
+import Data.Enumerator (Enumerator) -- enumerator
 -- class Eq a => ActionSymbol a
 data ActionSymbol = Index | List | Get | Read | Modify | Edit | Create | New | Delete | Destroy deriving Enum
 toActionSym i = toEnum i :: ActionSymbol
 
 -- instance ActionSymbol DefaultActionSymbol
+
+--findRecord :: (Monad m, PS.ToBackendKey SqlBackend record) => (SqlPersistT IO (Maybe record) -> m (Maybe record)) -> PS.Key record -> m (Maybe record)
+--findRecord db key = do
+--  db $ PS.get key -- Sql.SqlPersistT IO (Maybe record)
+
+runDB :: MonadIO m => ConnectionPool -> SqlPersistT IO a -> m a
+runDB p action = liftIO $ runSqlPool action p
 
 class Controller a where
   new :: ConnectionPool -> a
@@ -19,6 +28,5 @@ class Controller a where
   afterAction sym c = do
     return ()
 
--- data (Controller c) => ControllerAction c = ControllerAction Int (c -> ActionM c) | Direct (ActionM ())
 data (Controller c) => ControllerAction c = ControllerAction Int (c -> ActionM c)
 
--- shinjirecs-api/src/controllers/Channels.hs
+++ shinjirecs-api/src/controllers/Channels.hs
@@ -4,35 +4,60 @@ module Controllers.Channels where
 import Controller(Controller(..), ControllerAction(..))
 import Controller(ActionSymbol(List), toActionSym)
 
--- import Web.Scotty (ScottyM, scotty, status, json, get, patch, delete , post, options, ActionM, param, jsonData, addroute, setHeader,middleware, RoutePattern)
 import Web.Scotty (ActionM)
-import Control.Monad.IO.Class(MonadIO) -- base
+import Control.Monad.IO.Class(MonadIO,liftIO) -- base
 import qualified Database.Persist as P --persistent
 import qualified DB
 import qualified Database.Persist.Class as PS
-import Database.Persist.Sql(ConnectionPool)  --persistent
+import Database.Persist.Sql(ConnectionPool, SqlPersistT, runSqlPool)  --persistent
 
 -- MonadIO m => Sql.ConnectionPool -> Sql.SqlPersistT IO a -> m a
 -- PS.ToBackendKey SqlBackend record
--- data (MonadIO m, PS.ToBackendKey SqlBackend record) => Channels = Channels {db :: (Sql.SqlPersistT IO (ActionM (Maybe record)) -> m (ActionM (Maybe record))) }
-data Channels = Channels { conn :: ConnectionPool }
+data (PS.PersistEntity record) => Model record = Model {
+  find :: (SqlPersistT IO (ActionM (Maybe record)) -> ActionM (ActionM (Maybe record)))
+  }
+
+-- class (PS.PersistEntity record) => ModelClass record where
+--   finder :: (MonadIO m) => ConnectionPool -> (SqlPersistT IO (m (Maybe record)) -> m (m (Maybe record)))
+
+-- instance ModelClass DB.Channel where
+--   finder conn = runDB conn
+
+data Models = Models {
+  channels :: Model DB.Channel
+  }
+
+getModel :: (PS.PersistEntity record) => ConnectionPool -> Model record
+getModel conn = Model {
+  find = runDB conn
+  }
+
+getModels :: ConnectionPool -> Models
+getModels conn = Models {
+  channels = getModel conn
+  }
+
+data Channels = Channels { conn :: ConnectionPool, models :: Models }
 
 -- before :: Channels -> DefaultActionSymbol -> (Bool, Channels)
 before :: Channels -> ActionSymbol -> (Bool, Channels)
 before c List = (True, c)
 before c _    = (True, c)
 
+runDB :: MonadIO m => ConnectionPool -> SqlPersistT IO a -> m a
+runDB p action = liftIO $ runSqlPool action p
+
 instance Controller Channels where
-  new conn' = Channels { conn = conn'}
+  new conn' = Channels { conn = conn', models = getModels conn' }
   beforeAction i c = do
     return $ before c $ toActionSym i
     
 list = ControllerAction (fromEnum List) (
-  \c@Channels { conn = _ } -> do
+  \c -> do
     let filter = [] :: [P.Filter DB.Channel]
         opt    = [] :: [P.SelectOpt DB.Channel]
 --    records <- db $ map P.entityVal <$> P.selectList filter opt
 --    json records
-    return c
+    return (c :: Channels)
   )
 
