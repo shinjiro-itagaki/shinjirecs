aa4d25eb644f750fb0970942e67308cffab28df0
Shinjiro Itagaki <ishinjiroster@gmail.com>
2017-02-16 04:24:39 +0900

    modify model

--- shinjirecs-api/src/Model.hs
+++ shinjirecs-api/src/Model.hs
@@ -44,47 +44,69 @@ let resevation = findById conn $ param "id"
 res <- save conn reservation [RerservationStart_time +=. ]
 -}
 
-find :: (PS.PersistEntity entity, PS.ToBackendKey SqlBackend entity, PS.PersistRecordBackend entity SqlBackend, Read id, Show id) => id -> SqlPersistT IO (Maybe (Entity entity))
-
-find id = do
-  mEntity <- PS.get key 
-  return $ (case mEntity of
-               Just entity -> Just Entity {entityKey = key, entityVal = entity}
-               Nothing     -> Nothing )
-  where
-    ifNothing' = -1
-    key = toSqlKey $ fromMaybe ifNothing' $ readMaybe $ show id
-
-data SaveType = Update | Insert
+data SaveType = Modify | Create
 
 class (PS.PersistEntity entity, PS.ToBackendKey SqlBackend entity, PS.PersistRecordBackend entity SqlBackend) => ActiveRecord entity where
+
+  -- please override if you need
+  afterFind :: entity -> ReaderT SqlBackend IO entity
+  afterFind = return
+  
   -- please override if you need
   beforeSave :: SaveType -> entity -> ReaderT SqlBackend IO (Maybe entity)
   beforeSave _ = return . Just
 
+  -- please override if you need
   afterSave :: SaveType -> entity -> ReaderT SqlBackend IO (Maybe entity)
   afterSave _ = return . Just
   
   -- please override if you need
-  beforeInsertSave :: entity -> ReaderT SqlBackend IO (Maybe entity)
-  beforeInsertSave = return . Just
+  beforeCreate :: entity -> ReaderT SqlBackend IO (Maybe entity)
+  beforeCreate = return . Just
 
   -- please override if you need
-  beforeUpdateSave :: entity -> ReaderT SqlBackend IO (Maybe entity)
-  beforeUpdateSave = return . Just
+  afterCreate :: entity -> ReaderT SqlBackend IO (Maybe entity)
+  afterCreate = return . Just  
+
+  -- please override if you need
+  beforeModifySave :: entity -> ReaderT SqlBackend IO (Maybe entity)
+  beforeModifySave = return . Just
+
+  -- please override if you need
+  afterModifySave :: entity -> ReaderT SqlBackend IO (Maybe entity)
+  afterModifySave = return . Just
 
   -- please override if you need
   beforeDestroy :: entity -> ReaderT SqlBackend IO (Maybe entity)
   beforeDestroy = return . Just
+
+  -- please override if you need
+  afterDestroy :: entity -> ReaderT SqlBackend IO ()
+  afterDestroy self = return ()
+
+  afterCommit :: entity -> ReaderT SqlBackend IO ()
+  afterCommit self = return ()
+  
+  afterRollback :: entity -> ReaderT SqlBackend IO ()
+  afterRollback self = return ()
   
   existOnDb :: PS.Key entity -> ReaderT SqlBackend IO Bool
   existOnDb key = (return . maybeToBool) =<< (PS.liftPersist $ PS.get key)
   
   existOnDbBy :: entity -> ReaderT SqlBackend IO Bool
   existOnDbBy entity= (return . maybeToBool) =<< (PS.liftPersist . PS.getBy) =<< (PS.liftPersist $ PS.onlyUnique entity)
-  
-  saveAsUpdate :: PS.Key entity -> entity -> ReaderT SqlBackend IO (Maybe (Entity entity))
-  saveAsUpdate key entity = do
+
+  {-
+  beforeValidation
+  afterValidation
+  beforeSave
+  aroundSave
+  beforeModify
+  afterModify
+  afterSave
+  -}
+  modify :: PS.Key entity -> entity -> ReaderT SqlBackend IO (Maybe (Entity entity))
+  modify key entity = do
     PS.replace key entity
     mEntity <- PS.liftPersist $ PS.get key
     
@@ -93,9 +115,17 @@ class (PS.PersistEntity entity, PS.ToBackendKey SqlBackend entity, PS.PersistRec
     return $ (case mEntity of
                 Just entity2 -> Just Entity {entityKey = key, entityVal = entity2 }
                 Nothing -> Nothing)
-      
-  saveAsInsert :: entity -> ReaderT SqlBackend IO (Maybe (Entity entity))
-  saveAsInsert entity = do
+
+  {-
+  beforeValidation
+  afterValidation
+  beforeSave
+  beforeCreate
+  afterCreate
+  afterSave
+  -}
+  create :: entity -> ReaderT SqlBackend IO (Maybe (Entity entity))
+  create entity = do
     
     -- need to check failed(exception thrown) here
     
@@ -104,22 +134,11 @@ class (PS.PersistEntity entity, PS.ToBackendKey SqlBackend entity, PS.PersistRec
     return $ (case mEntity of
                 Just entity2 -> Just Entity {entityKey = key, entityVal = entity2 }
                 Nothing      -> Nothing )
-
-class (PS.PersistEntity entity, PS.ToBackendKey SqlBackend entity, PS.PersistRecordBackend entity SqlBackend) => (ActiveRecordKeyVal entity) record where
-  getKey :: record -> Maybe (PS.Key entity)
-  getVal :: record -> entity
-
-instance (ActiveRecord entity) => (ActiveRecordKeyVal entity) entity where
-  getKey self = Nothing
-  getVal self = self
-  
-instance (ActiveRecord entity) => ActiveRecordKeyVal entity (Entity entity) where  
-  getKey = Just . entityKey
-  getVal = entityVal
-  
+      
 -- need MultiParamTypeClasses
 -- need AllowAmbiguousTypes
 class (PS.PersistEntity entity, PS.ToBackendKey SqlBackend entity, PS.PersistRecordBackend entity SqlBackend) => (ActiveRecordSaver entity) record where
+  
   saveWithoutHook :: record -> ReaderT SqlBackend IO (Maybe (Entity entity))
   exist           :: record -> ReaderT SqlBackend IO Bool
   save :: record -> ReaderT SqlBackend IO (Maybe (Entity entity))
@@ -130,23 +149,38 @@ class (PS.PersistEntity entity, PS.ToBackendKey SqlBackend entity, PS.PersistRec
       Just val2 -> saveWithoutHook val2
       Nothing   -> return Nothing
     where
-      maybeToSaveType' Nothing = Insert
-      maybeToSaveType' (Just x)= Update
+      maybeToSaveType' Nothing = Create
+      maybeToSaveType' (Just x)= Modify
   
 class (PS.PersistEntity entity, PS.ToBackendKey SqlBackend entity, PS.PersistRecordBackend entity SqlBackend) => (ActiveRecordDestroyer entity) record where
   destroyWithoutHook :: record -> ReaderT SqlBackend IO (Maybe (PS.Key entity))
-  destroy            :: record -> ReaderT SqlBackend IO (Maybe (PS.Key entity))
-  
+
+  {-
+  before_destroy
+  after_destroy
+  -}
+  destroy :: record -> ReaderT SqlBackend IO (Maybe (PS.Key entity))
+
+findByKey :: (ActiveRecord entity) => PS.Key entity -> ReaderT SqlBackend IO (Maybe (Entity entity))
+findByKey key = do
+  mEntity <- PS.get key
+  case mEntity of
+    Just entity -> (\val' -> return $ Just $ Entity key val') =<< afterFind entity
+    Nothing     -> return Nothing
+    
+find :: (Read id, Show id, ActiveRecord entity) => id -> ReaderT SqlBackend IO (Maybe (Entity entity))
+find = let ifNothing' = -1 in findByKey . toSqlKey . fromMaybe ifNothing' . readMaybe . show
+          
 instance (PS.PersistEntity entity, PS.ToBackendKey SqlBackend entity, PS.PersistRecordBackend entity SqlBackend) => ActiveRecord entity
 
 instance (ActiveRecord entity) => (ActiveRecordSaver entity) entity where
   exist = existOnDbBy
-  saveWithoutHook = saveAsInsert
+  saveWithoutHook = create
   save self = saveByKeyVal self Nothing self
 
 instance (ActiveRecord entity) => ActiveRecordSaver entity (Entity entity) where
   exist = existOnDb . entityKey
-  saveWithoutHook self = saveAsUpdate (entityKey self) (entityVal self)
+  saveWithoutHook self = modify (entityKey self) (entityVal self)
   save self@(Entity key val) = saveByKeyVal self (Just key) val
 
 instance (ActiveRecord entity) => ActiveRecordDestroyer entity (Entity entity) where
@@ -163,7 +197,7 @@ instance (ActiveRecord entity) => ActiveRecordDestroyer entity (Entity entity) w
         -- check destroyed here
         return res
       Nothing -> return Nothing
-      
+
 class (PS.PersistEntity record, PS.DeleteCascade record SqlBackend) => CascadeDeletable record where
   deleteCascade      :: (MonadIO m) => ConnectionPool -> PS.Key record -> m ()
   deleteCascadeWhere :: (MonadIO m) => ConnectionPool -> [Filter record] -> m ()  
