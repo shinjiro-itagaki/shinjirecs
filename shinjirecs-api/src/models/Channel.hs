ece76e0a927e3ade45089ca51b9e9a57ce411d9a
Shinjiro Itagaki <ishinjiroster@gmail.com>
2017-02-23 11:39:25 +0900

    modify model

--- shinjirecs-api/src/models/Channel.hs
+++ shinjirecs-api/src/models/Channel.hs
@@ -2,7 +2,7 @@
 module Models.Channel where
 import DB(Channel)
 import Model(ActiveRecord(..))
-instance ActiveRecord Channel -- where
+-- instance ActiveRecord Channel -- where
 --  beforeSave _ = return . Just
 
 -- instance UniqueReplaceableModel Channel
