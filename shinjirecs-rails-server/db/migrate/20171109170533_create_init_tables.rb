35370874813a7efcb83d80c0d006f9f8086dd2da
Shinjiro Itagaki <ishinjiroster@gmail.com>
2017-11-10 06:04:39 +0900

    modify migrations

--- shinjirecs-rails-server/db/migrate/20171109170533_create_init_tables.rb
+++ shinjirecs-rails-server/db/migrate/20171109170533_create_init_tables.rb
@@ -1,47 +1,90 @@
 class CreateInitTables < ActiveRecord::Migration[5.1]
   def change
+
+    create_table :areas, unsigned: true do |t|
+      t.string  "label"            , null: false
+      t.boolean "channels_checked" , null: false, default: false
+      t.timestamps                   null: false
+      t.index :label, unique: true
+    end
+
+    create_table :system, unsigned: true do |t|
+      t.integer "area_id"      , null: false, default: 0, foreign_key: {on_delete: :ristrict, on_update: :cascade}
+      t.boolean "active"       , null: false, default: true
+      t.timestamps               null: false
+    end
+    execute "ALTER TABLE system ADD CONSTRAINT system_active CHECK( active = true );"
+
     create_table :channels, unsigned: true do |t|
       t.integer "number"       , null: false
+      t.integer "area_id"      , null: false, default: 0, foreign_key: {on_delete: :ristrict, on_update: :cascade}
       t.string  "ctype"        , null: false, default: "gr"
       t.string  "display_name" , null: false
       t.integer "order"        , null: false, default: 0
       t.timestamps               null: false
+      t.index ["area_id","number","ctype"], unique: true
+      t.index ["area_id","display_name"], unique: true
     end
-    execute "ALTER TABLE channels     ADD CONSTRAINT chk_channel_ctype     CHECK( ctype IN ('gr','bs'));"
+    execute "ALTER TABLE channels ADD CONSTRAINT chk_channel_ctype  CHECK( ctype IN ('gr','bs'));"
+    execute "ALTER TABLE channels ADD CONSTRAINT chk_channel_number CHECK( number > 0 );"
 
     create_table :program_categories, unsigned: true do |t|
       t.string     "label" , null: false
       t.timestamps           null: false
+      t.index "label", unique: true
     end
 
     create_table :programs, unsigned: true do |t|
       t.timestamp  "start_time"           , null: false
       t.timestamp  "stop_time"            , null: false
-      t.integer    "channel_id"           , null: false , foreign_key: {on_delete: :cascade}
+      t.integer    "channel_id"           , null: false , foreign_key: {on_delete: :cascade, on_update: :cascade} # delete all programs if channel is deleted
       t.string     "title"                , null: false
       t.text       "desc"                 , null: false
       t.integer    "event_id"             , null: false , default: 0
-      t.integer    "program_category_id"  , null: false , foreign_key: true
+      t.integer    "program_category_id"  , null: false , default: 0, foreign_key: {on_delete: :set_default, on_update: :cascade}
       t.timestamps                          null: false
+      t.index ["start_time", "channel_id"], unique: true
     end
+    execute "ALTER TABLE programs ADD CONSTRAINT chk_times CHECK( start_time < stop_time );"
+
+    create_table :program_titles, unsigned: true do |t|
+      t.timestamp  "start_time"           , null: false
+      t.timestamp  "end_time"             , null: false
+      t.integer    "channel_id"           , null: false , foreign_key: {on_delete: :ristrict, on_update: :cascade}
+      t.string     "title"                , null: false
+      t.text       "desc"                 , null: false
+      t.integer    "program_category_id"  , null: false , default: 0, foreign_key: {on_delete: :set_default, on_update: :cascade}
+      t.integer    "next_counter"         , null: false , default: 1
+      t.integer    "weekdays"             , null: false , default: 0, limit: 1 # byte
+      t.boolean    "auto_next"            , null: false , default: true
+      t.timestamps                          null: false
+      t.index ["start_time", "channel_id"], unique: true
+    end
+    execute "ALTER TABLE programs ADD CONSTRAINT chk_between CHECK( start_time < end_time )"
+    execute "ALTER TABLE programs ADD CONSTRAINT chk_weekdays CHECK( 0 <= weekdays and weekdays <= 127 )" # between ( 0b0000000 , 0b1111111 )
+    execute "ALTER TABLE programs ADD CONSTRAINT chk_next_counter CHECK( next_counter > 0 )"
 
     create_table :reservations, unsigned: true do |t|
       t.timestamp  "start_time"          , null: false
-      t.integer    "duration"            , null: false
+      t.integer    "duration"            , null: false # seconds
       t.integer    "channel"             , null: false , foreign_key: true
       t.string     "title"               , null: false
       t.text       "desc"                , null: false
       t.integer    "event_id"            , null: false , default: 0
-      t.integer    "program_category_id" , null: false , foreign_key: true
+      t.integer    "program_category_id" , null: false , default: 0, foreign_key: {on_delete: :set_default, on_update: :cascade}
+      t.integer    "program_title_id"    , null: false , default: 0, foreign_key: {on_delete: :set_default, on_update: :cascade}
+      t.integer    "counter"             , null: false , default: 0
 
       # -2: canceled
       # -1: failed
       #  0: waiting
       #  1: recording
       #  2: success
-      t.integer    "state"               , null: false , default: 0
+      t.integer    "state"               , null: false , default: 0, limit: 1
       t.timestamps                         null: false
     end
     execute "ALTER TABLE reservations ADD CONSTRAINT chk_reservation_state CHECK( state IN (-2,-1,0,1,2));"
+    execute "ALTER TABLE reservations ADD CONSTRAINT chk_reservation_duration CHECK( duration > 0);"
+    execute "ALTER TABLE reservations ADD CONSTRAINT chk_reservation_duration CHECK( counter >= 0);"
   end
 end
--- shinjirecs-rails-server/db/schema.rb
+++ shinjirecs-rails-server/db/schema.rb
@@ -12,19 +12,46 @@
 
 ActiveRecord::Schema.define(version: 20171109170533) do
 
+  create_table "areas", id: :bigint, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "label", null: false
+    t.boolean "channels_checked", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["label"], name: "index_areas_on_label", unique: true
+  end
+
   create_table "channels", id: :bigint, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "number", null: false
+    t.integer "area_id", default: 0, null: false
     t.string "ctype", default: "gr", null: false
     t.string "display_name", null: false
     t.integer "order", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["area_id", "display_name"], name: "index_channels_on_area_id_and_display_name", unique: true
+    t.index ["area_id", "number", "ctype"], name: "index_channels_on_area_id_and_number_and_ctype", unique: true
   end
 
   create_table "program_categories", id: :bigint, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "label", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["label"], name: "index_program_categories_on_label", unique: true
+  end
+
+  create_table "program_titles", id: :bigint, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.timestamp "start_time", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.timestamp "end_time", null: false
+    t.integer "channel_id", null: false
+    t.string "title", null: false
+    t.text "desc", null: false
+    t.integer "program_category_id", default: 0, null: false
+    t.integer "next_counter", default: 1, null: false
+    t.integer "weekdays", limit: 1, default: 0, null: false
+    t.boolean "auto_next", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["start_time", "channel_id"], name: "index_program_titles_on_start_time_and_channel_id", unique: true
   end
 
   create_table "programs", id: :bigint, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -34,9 +61,10 @@ ActiveRecord::Schema.define(version: 20171109170533) do
     t.string "title", null: false
     t.text "desc", null: false
     t.integer "event_id", default: 0, null: false
-    t.integer "program_category_id", null: false
+    t.integer "program_category_id", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["start_time", "channel_id"], name: "index_programs_on_start_time_and_channel_id", unique: true
   end
 
   create_table "reservations", id: :bigint, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -46,8 +74,17 @@ ActiveRecord::Schema.define(version: 20171109170533) do
     t.string "title", null: false
     t.text "desc", null: false
     t.integer "event_id", default: 0, null: false
-    t.integer "program_category_id", null: false
-    t.integer "state", default: 0, null: false
+    t.integer "program_category_id", default: 0, null: false
+    t.integer "program_title_id", default: 0, null: false
+    t.integer "counter", default: 0, null: false
+    t.integer "state", limit: 1, default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "system", id: :bigint, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "area_id", default: 0, null: false
+    t.boolean "active", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
