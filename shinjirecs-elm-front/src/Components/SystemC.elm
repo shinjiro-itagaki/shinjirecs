95d9dee0f60ada45ab33736b65446ddd35fea39b
Shinjiro Itagaki <ishinjiroster@gmail.com>
2017-11-22 10:51:24 +0900

    modify trial

--- shinjirecs-elm-front/src/Components/SystemC.elm
+++ shinjirecs-elm-front/src/Components/SystemC.elm
@@ -205,11 +205,47 @@ accept tipe m =
     let html =
             case tipe of
                 IndexAction   -> DirectMsg m indexView2
-                ShowAction    -> DirectMsg m showView2
-                EditAction    -> DirectMsg m editView2
+                ShowAction    -> execShowAction2 m
+                EditAction    -> execEditAction2 m
                 ModifyAction  -> redirectTo2 (ToSystemReq ShowAction)
     in html
 
+execEditAction2 : Models -> RootMsg2
+execEditAction2 m = HasCmd <| Cmd.map execEditAction2Impl <| loadSchemaAction2 m
+        
+execEditAction2Impl : Models -> RootMsg2 -- HasCmd (Cmd (DirectMsg Models (Models -> Html RootMsg2))) 
+execEditAction2Impl m =
+    let smodel = m.system
+        r  = m.readonly
+        rw = m.editable
+    in Cmd.map (\res -> (case res of
+                            Ok newrec   -> DirectMsg {m|system   = {smodel| edit_record = Just newrec}}
+                            Err httperr -> DirectMsg {m|editable = {rw| errmsg = r.httpErrorToString httperr }}
+                        ) editView2
+               ) m.readonly.api.system.get |> HasCmd
+
+loadSchemaAction2 : Models -> Cmd Models
+loadSchemaAction2 m =
+    let smodel = m.system
+        r  = m.readonly
+        rw = m.editable
+    in Cmd.map (\res -> (case res of
+                            Ok newrec   -> {m|system   = {smodel| system_schema = Just newrec}}
+                            Err httperr -> {m|editable = {rw| errmsg = r.httpErrorToString httperr }}
+                        )
+               ) m.readonly.api.system.info
+        
+execShowAction2 : Models -> RootMsg2
+execShowAction2 m =
+    let smodel = m.system
+        r  = m.readonly
+        rw = m.editable
+    in Cmd.map (\res -> (case res of
+                            Ok newrec   -> DirectMsg {m|system   = {smodel| show_record = Just newrec}}
+                            Err httperr -> DirectMsg {m|editable = {rw| errmsg = r.httpErrorToString httperr }}
+                        ) showView2
+               ) m.readonly.api.system.get |> HasCmd
+        
 indexView2 : Models -> Html RootMsg2
 indexView2 m =
     div [] [
@@ -221,13 +257,37 @@ indexView2 m =
 showView2 : Models -> Html RootMsg2
 showView2 m =
     div [] [
-         H.span [] [text <| "show"]
+         H.span [] [text <| if m.system.show_record == Nothing then "ロード失敗した" else "ロード済み"]
         ,button [onClick <| SendRequest <| ToSystemReq IndexAction] [text "システム情報インデックスへ"]
         ]
 
 editView2 : Models -> Html RootMsg2
 editView2 m =
     div [] [
-         H.span [] [text <| "edit"]
-        ,button [onClick <| SendRequest <| ToSystemReq IndexAction] [text "システム情報インデックスへ"]
-        ]        
+         H.div [] [text <| "システム編集"]
+        ,(case m.system.system_schema of
+              Just rec -> editViewIfSystemSchemaLoaded rec
+              Nothing  -> editViewIfSystemSchemaNotFound) m.system
+        ,button [onClick <| SendRequest <| ToSystemReq IndexAction] [text "システム情報インデックスへ"] 
+        ]
+
+editViewIfSystemSchemaNotFound : SystemModel -> Html RootMsg2
+editViewIfSystemSchemaNotFound m =
+    div [] [
+         H.div [] [text <| "スキーマがロードされていません"]
+        ]
+editViewIfSystemSchemaLoaded : Dict String ColumnInfo -> SystemModel -> Html RootMsg2
+editViewIfSystemSchemaLoaded scm m =
+    (case  m.edit_record of
+         Just rec -> editViewIfRecordLoaded rec
+         Nothing  -> editViewIfRecordNotFound) m
+
+editViewIfRecordNotFound : SystemModel -> Html RootMsg2
+editViewIfRecordNotFound m =
+    div [] [
+         H.div [] [text <| "データがロードされていません"]
+        ]
+editViewIfRecordLoaded : Entity System -> SystemModel -> Html RootMsg2
+editViewIfRecordLoaded rec m =
+    div [] [
+        ]
