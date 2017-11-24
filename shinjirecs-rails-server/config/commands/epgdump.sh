da9edff691ed413b7e9defc618d78409145a75ca
Shinjiro Itagaki <ishinjiroster@gmail.com>
2017-11-24 12:59:35 +0900

    modify epgdump

--- shinjirecs-rails-server/config/commands/epgdump.sh
+++ shinjirecs-rails-server/config/commands/epgdump.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env sh
-ch=$1
-sec=$2
-if [ "$sec" = "" ]
-then
-    sec=10
-fi
 
-recpt1 ${ch} ${sec} - | epgdump - -
+# usage
+# epgdump.sh ./in.ts ./temp.json
+#   or
+# epgdump.sh 13 10 ./temp.json
+in_=$1
+out=$2
+
+if [ -f ${in_} ] ; then
+    epgdump json ${in_} ${out}
+else # ${in} is channel number
+    ch=$1
+    sec=$3
+    if [ "$sec" = "" ]
+    then
+        sec=10
+    fi
+    recpt1 ${ch} ${sec} - | epgdump json - ${out}
+fi           
