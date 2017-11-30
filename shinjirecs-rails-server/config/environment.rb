231fad85dfb134142c186cc3f934bb19a98dfc47
Shinjiro Itagaki <ishinjiroster@gmail.com>
2017-11-30 22:44:35 +0900

    modify thread

--- shinjirecs-rails-server/config/environment.rb
+++ shinjirecs-rails-server/config/environment.rb
@@ -7,8 +7,12 @@ Rails.application.initialize!
 OBSERVER_THREAD = Thread.new do
   while true
     begin
-      Reservation.check_staging
-      sleep 1
+      if ActiveRecord::Base.connection.pool.connected? then
+        Reservation.check_staging
+        sleep 1
+      else
+        sleep 3
+      end
     rescue => e
       puts e.message
     end
