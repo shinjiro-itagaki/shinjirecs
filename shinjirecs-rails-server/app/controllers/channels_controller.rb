2696ba7e1acb8d9cf5803e289c216b24f0adab0b
Shinjiro Itagaki <ishinjiroster@gmail.com>
2017-11-30 18:02:21 +0900

    modify for thread running of channels/scam

--- shinjirecs-rails-server/app/controllers/channels_controller.rb
+++ shinjirecs-rails-server/app/controllers/channels_controller.rb
@@ -3,74 +3,41 @@ require 'open3'
 require 'tempfile'
 
 class ChannelsController < ApplicationController
+  class ScanThread < Thread
+    attr_accessor :cancel
+  end
+
   set_model Channel
   def self.permitted_params
     [:number,:area_id,:ctype,:display_name,:display_name_locked,:order]
   end
 
-  def self.scan_timeout
-    5
-  end
+  @@scan_thread=nil
 
   def scan
-    timeout_sec = params[:timeout].to_s.to_i
-    timeout_sec = self.class.scan_timeout if timeout_sec < 1
-
-    gr = params[:gr] || Channel.default_all_gr_numbers
-    bs = params[:bs] || Channel.default_all_bs_numbers
-    area_id = System.instance.area_id
-    cmdfile = Command.scan_channel_cmd
-    cmdfilepath = cmdfile.path
-
-    case cmdfile
-    when Command::GetCommandPathResult::GetSuccess
-    when Command::GetCommandPathResult::NotFound
-      puts cmdfilepath + " is not found."
-      render_data nil
-      return
-    when Command::GetCommandPathResult::NotExecutable
-      puts cmdfilepath + " is not executable."
-      render_data nil
-      return
-    end
-
-    {"gr" => gr,"bs" => bs}.each do |type,charr|
-      charr = charr.map {|ch|
-        c = Channel.find_or_initialize_by(number: ch, area_id: area_id, ctype: type)
-        if c.new_record?
-          c.scaned = false
-          c.save!
-        end
-        c
-      }
-
-      charr.each do |ch|
-        tempfile = Tempfile.new('for-scan')
-        tempfilepath = tempfile.path
-        cmd = "#{cmdfilepath} #{ch.number} #{timeout_sec} #{tempfilepath}"
-        tempfile.unlink
-
-        puts cmd
-        res = false
-        scaned = true
-        # io = IO.popen(cmd, "r")
-        pid = spawn(cmd, pgroup: Process.pid)  # io.pid
-        puts "command pid=#{pid}"
-        watch_thread = Process.detach(pid)
-        watch_thread.join
-
-        if File.exists?(tempfilepath) then
-          puts "command success  ......... channel '#{ch.number}' was DETECTED!! "
-          ch.exist = true
-        else
-          puts "command failed. Channel '#{ch.number}' was not found."
-          ch.exist = false
-        end
-        ch.scaned = scaned
-        puts "before save=="
-        ch.save!
+    if @@scan_thread then
+      if params["cancel"] then
+        @@scan_thread.cancel = true
+        @@scan_thread.exit
+        @@scan_thread = nil
+        render_data canceled: true
+      else
+        render_data working: true
       end
     end
+
+    @@scan_thread ||= ScanThread.start do
+      ActiveRecord::Base.connection_pool.with_connection do
+        Channel.scan params[:gr], params[:bs], params[:timeout] do
+          if (th = Thread.current).kind_of? ScanThread then
+            th.canceled
+          else
+            false
+          end
+        end
+      end
+    end
+    render_data start: true
   end
 
   protected
--- shinjirecs-rails-server/app/models/channel.rb
+++ shinjirecs-rails-server/app/models/channel.rb
@@ -98,6 +98,88 @@ class Channel < ApplicationRecord
     self.service_id > 0
   end
 
+  def self.scan_timeout
+    5
+  end
+
+  def self.scan(gr=nil,bs=nil,timeout=nil,&block)
+    gr ||= self.default_all_gr_numbers
+    bs ||= self.default_all_bs_numbers
+    timeout_sec = timeout.to_s.to_i
+    timeout_sec = self.scan_timeout if timeout_sec < 1
+
+    area_id = System.instance.area_id
+    cmdfile = Command.scan_channel_cmd
+    cmdfilepath = cmdfile.path
+
+    case cmdfile
+    when Command::GetCommandPathResult::GetSuccess
+    when Command::GetCommandPathResult::NotFound
+      puts cmdfilepath + " is not found."
+      render_data nil
+      return
+    when Command::GetCommandPathResult::NotExecutable
+      puts cmdfilepath + " is not executable."
+      render_data nil
+      return
+    end
+    if block and block.call then
+      return true
+    end
+    {"gr" => gr,"bs" => bs}.each do |type,charr|
+      charr = charr.map {|ch|
+        c = Channel.find_or_initialize_by(number: ch, area_id: area_id, ctype: type)
+        if c.new_record?
+          c.scaned = false
+          c.save!
+        end
+        c
+      }
+
+      charr.each do |ch|
+        tempfile = Tempfile.new('for-scan')
+        tempfilepath = tempfile.path
+        cmd = "#{cmdfilepath} #{ch.number} #{timeout_sec} #{tempfilepath}"
+        tempfile.unlink
+
+        puts cmd
+        res = false
+        scaned = true
+        # io = IO.popen(cmd, "r")
+        if block and block.call then
+          return true
+        end
+        pid = spawn(cmd, pgroup: Process.pid)  # io.pid
+        puts "command pid=#{pid}"
+        watch_thread = Process.detach(pid)
+        term = false
+        if block and block.call then
+          term = true
+          Process.kill Signal.list["TERM"], pid
+        end
+        watch_thread.join
+
+        if block and block.call then
+          return true
+        end
+
+        if term then
+          return true
+        end
+
+        if File.exists?(tempfilepath) then
+          puts "command success  ......... channel '#{ch.number}' was DETECTED!! "
+          ch.exist = true
+        else
+          puts "command failed. Channel '#{ch.number}' was not found."
+          ch.exist = false
+        end
+        ch.scaned = scaned
+        ch.save!
+      end
+    end
+  end
+
   private
   def set_default
     self.ctype        ||= 'gr'
