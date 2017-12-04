3247208cf57d367b1d77071444c46ade3c90cdb5
Shinjiro Itagaki <ishinjiroster@gmail.com>
2017-12-04 16:49:03 +0900

    modify recording command

--- shinjirecs-rails-server/config/commands/recording.sh
+++ shinjirecs-rails-server/config/commands/recording.sh
@@ -2,7 +2,16 @@
 ch=$1
 sec=$2
 out=$3
-(recpt1 --b25 --strip ${ch} ${sec} ${out} 1>&2) &
-# (recpt1 --b25 --strip ${ch} ${sec} ${out}) &
-echo $!
-wait $!
+sid=$4
+opt_sid="--sid ${sid}"
+ffmpeg_opts=$5
+
+if [ "${sid}" = "" ] ; then
+    opt_sid=""
+fi
+
+(recpt1 ${opt_sid} --b25 --strip ${ch} ${sec} pipe:5 1>&2) &
+pid=$!
+echo $pid
+(cat pipe:5 | tee ${out} | ffmpeg -y ${ffmpeg_opts} -i - ${out}.mpeg 1>&2) &
+wait $pid      
