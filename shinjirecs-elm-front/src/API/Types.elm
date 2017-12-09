d2ea8276125b9d37fa4fe57b7e97044485725f29
Shinjiro Itagaki <ishinjiroster@gmail.com>
2017-12-09 18:50:06 +0900

    add async api methods

--- shinjirecs-elm-front/src/API/Types.elm
+++ shinjirecs-elm-front/src/API/Types.elm
@@ -1,6 +1,7 @@
 module API.Types exposing (API,AreasI,ChannelsI,EpgProgramsI,EpgProgramCategoriesI,ProgramSeriesI,ReservationsI,SystemI,ResourcesI,Cache,emptyCache)
 import Dict exposing (Dict)
 import Http
+import Http.Progress exposing (Progress)
 import Records.Types exposing (Entity)
 import Records.ColumnInfo exposing (ColumnInfo,columnInfoDecoder)
 import Records.Area exposing (Area)
@@ -59,6 +60,12 @@ type alias ResourcesI a =
     , modify  : Entity a  -> Cmd (Result Http.Error a)
     , destroy : Entity a  -> Cmd (Result Http.Error Bool)
     , info    : Cmd (Result Http.Error (Dict String ColumnInfo))
+    , indexAsync   : Maybe Int -> (Progress (List (Entity a)) -> Result Http.Error (List (Entity a)))  -> Sub (Result Http.Error (List (Entity a))) -- Int => limit
+    , getAsync     : Int       -> (Progress a                 -> Result Http.Error a)                  -> Sub (Result Http.Error a) 
+    , createAsync  : a         -> (Progress (Entity a)        -> Result Http.Error (Entity a))         -> Sub (Result Http.Error (Entity a)) 
+    , modifyAsync  : Entity a  -> (Progress a                 -> Result Http.Error a)                  -> Sub (Result Http.Error a)
+    , destroyAsync : Entity a  -> (Progress Bool              -> Result Http.Error Bool)               -> Sub (Result Http.Error Bool)
+    , infoAsync    : (Progress (Dict String ColumnInfo) -> Result Http.Error (Dict String ColumnInfo)) -> Sub (Result Http.Error (Dict String ColumnInfo))
     }
 
 type alias AreasI             = ResourcesI Area
@@ -71,4 +78,8 @@ type alias SystemI            = { get    : Cmd (Result Http.Error (Entity System
                                 , modify : Entity System -> Cmd (Result Http.Error System)
                                 , info   : Cmd (Result Http.Error (Dict String ColumnInfo))
                                 , all    : Cmd (Result Http.Error Cache)
+                                , getAsync    : (Progress (Entity System) -> Result Http.Error (Entity System)) -> Sub (Result Http.Error (Entity System))
+                                , modifyAsync : Entity System -> (Progress System -> Result Http.Error System) -> Sub (Result Http.Error System)
+                                , infoAsync   : (Progress (Dict String ColumnInfo) -> Result Http.Error (Dict String ColumnInfo)) -> Sub (Result Http.Error (Dict String ColumnInfo))
+                                , allAsync    : (Progress Cache -> Result Http.Error Cache) -> Sub (Result Http.Error Cache)
                                 }-- ResourcesI System
